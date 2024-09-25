0c2c78a45a9e65777b05c52c4524dbb3e00ce43a
nThzzzz <161968206+nThzzzz@users.noreply.github.com>
2024-09-25 19:24:31 -0300

    sexo 3

--- manip_exchange.h
+++ manip_exchange.h
@@ -5,17 +5,51 @@
 #include <string.h>
 #include <time.h>
 #include <stdlib.h>
+#include <ctype.h>//para usar o toupper nas escolhas
+#include <math.h>
 
-void menu(int usuariologado);
-void menuinicial(int usuariologado);
-void consultarsaldo(int usuariologado);
-void consultarextrato(int usuariologado);
-void depositar(int usuariologado);
-void sacar(int usuariologado);
-void comprar(int usuariologado);
-void vender(int usuariologado);
-void atualizar();
-void verificação(int usuariologado);
+
+typedef struct {
+    char CPF[12];
+    char senha[7];  
+    char nome[100];
+    float reais;
+    float btc;
+    float eth;
+    float xrp;
+    char ext[100][100];
+} pessoa;
+
+/*---------------------------------------PROTÓTIPOS MENUS---------------------------------------*/
+
+void menu(pessoa pessoas[], float cotacaoBTC, float cotacaoETH, float cotacaoXRP, int usuariologado);
+void menuinicial(pessoa pessoas[], float cotacaoBTC, float cotacaoETH, float cotacaoXRP, int usuariologado);
+
+/*---------------------------------PROTÓTIPOS FUNÇÕES PRINCIPAIS---------------------------------*/
+
+void login(pessoa pessoas[], float cotacaoBTC, float cotacaoETH, float cotacaoXRP, int usuariologado);
+void cadastrar(pessoa pessoas[], float cotacaoBTC, float cotacaoETH, float cotacaoXRP, int usuariologado);
+void consultarsaldo(pessoa pessoas[], int usuariologado);
+void consultarextrato(pessoa pessoas[], int usuariologado);
+void depositar(pessoa pessoas[], float cotacaoBTC, float cotacaoETH, float cotacaoXRP, int usuariologado);
+void sacar(pessoa pessoas[], float cotacaoBTC, float cotacaoETH, float cotacaoXRP, int usuariologado);
+void comprar(pessoa pessoas[], float cotacaoBTC, float cotacaoETH, float cotacaoXRP, int usuariologado);
+void vender(pessoa pessoas[], float cotacaoBTC, float cotacaoETH, float cotacaoXRP, int usuariologado);
+void atualizar(pessoa pessoas[],float cotacaoBTC, float cotacaoETH, float cotacaoXRP, int usuariologado);
+
+/*---------------------------------PROTÓTIPOS FUNÇÕES AUXILIARES---------------------------------*/
+
+void verificacao(pessoa pessoas[], int usuariologado);
 int verificaCPF(char *cpf);
+void printarcpf(char *cpf);
+void criaextrato(pessoa pessoas[], int usuariologado, char sinal, float valor, char moeda[6], float taxa);
+void limpaterminal();
+void espera();
+void recibocompra(float compra, float taxa);
+void recibovenda(float compra, float taxa);
+void ler(FILE *file, pessoa pessoas[], int quantidade, float *cotacaoBTC, float *cotacaoETH, float *cotacaoXRP);
+void escrever(pessoa pessoas[], int quantidade, float cotacaoBTC, float cotacaoETH, float cotacaoXRP);
+int arquivoexiste(const char *filename);
+void limpabuffer();
 
 #endif
